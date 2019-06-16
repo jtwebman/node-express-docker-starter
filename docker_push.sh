3123bc3fe95e82a688c897c3bd4a313d90e9058e
JT Turner <jt.turner@moovel.com>
2019-06-15 23:36:45 -0700

    Travis send to coverall again.

--- docker_push.sh
+++ docker_push.sh
@@ -5,3 +5,5 @@ PACKAGE_VERSION=$(grep '"version":' package.json | cut -d\" -f4)
 docker tag jtwebman/node-express-docker-starter "jtwebman/node-express-docker-starter:$PACKAGE_VERSION"
 docker push jtwebman/node-express-docker-starter:latest
 docker push "jtwebman/node-express-docker-starter:$PACKAGE_VERSION"
+
+docker run -it --rm -e TRAVIS_JOB_ID="$TRAVIS_JOB_ID" -e TRAVIS_BRANCH="$TRAVIS_BRANCH" -w /app -v $(pwd):/app: node:lts npm i && npm run coveralls
