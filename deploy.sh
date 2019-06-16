59d5e7014e79c3a1214d18d2c139652bed358a1e
JT Turner <jt.turner@moovel.com>
2019-06-16 09:44:58 -0700

    Build all versions deploy with job.

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,9 @@ PACKAGE_VERSION=$(grep '"version":' package.json | cut -d\" -f4)
 
 # Login to docker hub and push latest, dev, and version tags
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t jtwebman/node-express-docker-starter:dev --target=development .
+docker build -t jtwebman/node-express-docker-starter .
 docker tag jtwebman/node-express-docker-starter "jtwebman/node-express-docker-starter:$PACKAGE_VERSION"
 docker push jtwebman/node-express-docker-starter:latest
 docker push "jtwebman/node-express-docker-starter:$PACKAGE_VERSION"
 docker push jtwebman/node-express-docker-starter:dev
-
-# Run coverall to push coverage reports
-npm i && npm run coverall
