74c9d09f849d06ccac9f314f8acdc8edb1145130
JT Turner <jt.turner@moovel.com>
2019-06-15 22:59:45 -0700

    Make travis docker push version tag as well.

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push jtwebman/node-express-docker-starter
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+docker tag jtwebman/node-express-docker-starter jtwebman/node-express-docker-starter:$PACKAGE_VERSION
+docker push jtwebman/node-express-docker-starter:latest
+docker push jtwebman/node-express-docker-starter:$PACKAGE_VERSION
