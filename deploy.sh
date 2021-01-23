a270937eb65812f393ce95c930480366cade0b2b
JT Turner <jt.turner@azlo.com>
2021-01-23 10:45:02 -0800

    Stop make dev label docker on deploy.

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,3 @@ docker build -t jtwebman/node-express-docker-starter .
 docker tag jtwebman/node-express-docker-starter "jtwebman/node-express-docker-starter:$PACKAGE_VERSION"
 docker push jtwebman/node-express-docker-starter:latest
 docker push "jtwebman/node-express-docker-starter:$PACKAGE_VERSION"
-docker push jtwebman/node-express-docker-starter:dev
